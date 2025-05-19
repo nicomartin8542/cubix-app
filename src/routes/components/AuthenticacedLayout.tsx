98596c8d5789b9f8329cecde888ae7b39b78a328
Nicolas Martin <nicolasmartin@MacBook-Air-de-Nicolas.local>
2025-05-18 22:16:52 -0300

    arreglando

--- src/routes/components/AuthenticacedLayout.tsx
+++ src/routes/components/AuthenticacedLayout.tsx
@@ -1,8 +1,6 @@
 // components/AuthenticatedLayout.tsx
 import { Authenticated } from "@refinedev/core";
 import { CatchAllNavigate } from "@refinedev/react-router";
-import { Outlet } from "react-router-dom";
-import { Layout } from "@/layout";
 import { ROUTES } from "@/routes/config/constants";
 
 export const AuthenticatedLayout = ({
@@ -14,9 +12,6 @@ export const AuthenticatedLayout = ({
     key="authenticated-inner"
     fallback={<CatchAllNavigate to={ROUTES.LOGIN} />}
   >
-    <Layout>
-      <Outlet />
-      {children}
-    </Layout>
+    {children}
   </Authenticated>
 );
